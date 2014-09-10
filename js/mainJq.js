7b2bf3baec2a2d05c6cfbbf7ef57c9b0cec1a71b
mdwork <mazdze@gmail.com>
2014-09-10 11:07:30 +0300

    3 part

--- js/mainJq.js
+++ js/mainJq.js
@@ -3,9 +3,31 @@
     var subMenu = $('.nav li').children('ul');
     if(subMenu) {
         subMenu.parent().children('a').css({
-            'backgroundImage':'url(images/header/icon-triagle.png)',
+            'backgroundImage':'url(images/header/icon-triangle.png)',
             'paddingRight': 30 + 'px'
         });
     }
     /*end*/
+
+    /*toggle form*/
+    var toggleButton = $('.toggle-search-form'),
+        toggleButtonLink = $('.bt-toggle'),
+        toggleElementForm = $('.form-search-features');
+
+    toggleButton.on('click', function(e){
+        e.preventDefault();
+        toggleElementForm.slideToggle('slow', function(){
+            if(toggleButton.text() == 'Свернуть поиск') {
+                toggleButtonLink.text('Развернуть поиск');
+                toggleButton.css('backgroundImage','url(images/content/icon-triangle-disable.png)');
+            }
+            else {
+                toggleButtonLink.text('Свернуть поиск');
+                toggleButton.css('backgroundImage','url(images/content/icon-triangle-active.png)');
+            }
+        });
+    });
+    /*end*/
+
+
 })(jQuery);
