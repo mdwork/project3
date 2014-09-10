732b241c4c528ccecd7b026dc7074f82b02a14c5
mdwork <mazdze@gmail.com>
2014-09-10 14:17:36 +0300

    4 part

--- js/mainJq.js
+++ js/mainJq.js
@@ -29,5 +29,28 @@
     });
     /*end*/
 
+    /*toggle auto pages*/
+    $('.list-filter-auto a').on('click', function(e){
+        e.preventDefault();
+        var currentLink = $(e.target).text(),
+            li = $(e.target).parent(),
+            valueLi = $(e.target).parent().hasClass('active-category-auto'),
+            allItemFilter = $('.list-filter-auto li');
 
+        if(valueLi != 'active-category-auto') {
+            for(var i = 0; i < allItemFilter.length; i++) {
+                $(allItemFilter[i]).removeAttr('class');
+            }
+
+            li.addClass('active-category-auto');
+
+            if(currentLink == 'По марке') {
+                var brand = $('.list-auto-brand'),
+                    brandAttr = brand.attr('class'),
+                    arrayClass = brandAttr.split(' ');
+            }
+
+        }
+    });
+    /*end*/
 })(jQuery);
