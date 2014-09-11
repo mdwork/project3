c75c2d08846948bfb0d2124e2daa9e981123af6c
mdwork <mazdze@gmail.com>
2014-09-11 11:47:30 +0300

    7 part

--- js/mainJq.js
+++ js/mainJq.js
@@ -30,12 +30,12 @@
     /*end*/
 
     /*toggle auto pages*/
-    $('.list-filter-auto a').on('click', function(e){
+    $('#list-filter-auto_js a').on('click', function(e){
         e.preventDefault();
         var currentLink = $(e.target).text(),
             li = $(e.target).parent(),
             valueLi = $(e.target).parent().hasClass('active-category-auto'),
-            allItemFilter = $('.list-filter-auto li');
+            allItemFilter = $('#list-filter-auto_js li');
 
         if(valueLi != 'active-category-auto') {
             for(var i = 0; i < allItemFilter.length; i++) {
@@ -61,6 +61,46 @@
                 city.addClass('show_js');
             }
 
+        }
+    });
+    /*end*/
+
+    /*toggle autoservices pages*/
+    $('#list-autoservice-auto_js a').on('click', function(e){
+        e.preventDefault();
+        var currentLink = $(e.target).text(),
+            li = $(e.target).parent(),
+            valueLi = $(e.target).parent().hasClass('active-category-auto'),
+            allItemFilter = $('#list-autoservice-auto_js li');
+
+        if(valueLi != 'active-category-auto') {
+            for(var i = 0; i < allItemFilter.length; i++) {
+                $(allItemFilter[i]).removeAttr('class');
+            }
+
+            li.addClass('active-category-auto');
+
+            var sto = $('.list-sto').parent(),
+                rental = $('.list-car-rental').parent(),
+                dealer = $('.list-dealers').parent(),
+                trucking = $('.list-trucking').parent();
+
+            if(currentLink == 'СТО') {
+                $('.wrap-all-category-autoservice > li').removeAttr('class');
+                sto.addClass('show_js');
+            }
+            if(currentLink == 'Прокат авто') {
+                $('.wrap-all-category-autoservice > li').removeAttr('class');
+                rental.addClass('show_js');
+            }
+            if(currentLink == 'Автодиллеры') {
+                $('.wrap-all-category-autoservice > li').removeAttr('class');
+                dealer.addClass('show_js');
+            }
+            if(currentLink == 'Грузоперевозки') {
+                $('.wrap-all-category-autoservice > li').removeAttr('class');
+                trucking.addClass('show_js');
+            }
         }
     });
     /*end*/
