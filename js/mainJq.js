a7aa6ae1eee9f7e57b799c77cbab02f5623ec509
mdwork <mazdze@gmail.com>
2014-09-19 15:39:09 +0300

    19.09.2014

--- js/mainJq.js
+++ js/mainJq.js
@@ -282,14 +282,15 @@
         });
     });
 
-    $('.check-other-parameters').parent().on('click', function(e){
+    $('input.check-other-parameters').change('click', function(e){
         var curLabel = $(e.target).parent();
-        if(curLabel.hasClass('check-other-parameters')) {
-            curLabel.removeClass('check-other-parameters');
+
+        if(curLabel.hasClass('bg-label_js')) {
+            curLabel.removeClass('bg-label_js');
         }
-        else if (curLabel.hasClass('check-other-parameters')) {
-            curLabel.hasClass('check-other-parameters');
+        else if (!curLabel.hasClass('bg-label_js')) {
+            curLabel.addClass('bg-label_js');
         }
-        console.log(curLabel.hasClass('check-other-parameters'));
+
     });
 })(jQuery);
